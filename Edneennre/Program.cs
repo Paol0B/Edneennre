9da30a98c7fca4fdd7ed699cfc8c7c97345f3406
Alessandro Aletta <aletta@sirius-ea.com>
2024-11-29 11:17:58 +0100

    Prima organizzazione

--- Edneennre/Program.cs
+++ Edneennre/Program.cs
@@ -1,4 +1,23 @@
-﻿Console.WriteLine("Lento o veloce? (1, 2)");
+﻿///     V       V   AAAAA   RRRRR   IIIIIII   AAAAA   BBBBB  IIIIIII  L      IIIIIII
+///      V     V   A     A  R    R     I     A     A  B    B    I     L         I   
+///       V   V    AAAAAAA  RRRRR      I     AAAAAAA  BBBBB     I     L         I   
+///        V V     A     A  R   R      I     A     A  B    B    I     L         I   
+///         V      A     A  R    R  IIIIIII  A     A  BBBBB  IIIIIII  LLLLLL IIIIIII
+///     
+
+
+
+
+
+
+///     M       M   AAAAA   IIIIIII N     N
+///     MM     MM  A     A     I    NN    N
+///     M M   M M  AAAAAAA     I    N N   N
+///     M  M  M M  A     A     I    N  N  N
+///     M   M   M  A     A  IIIIIII N   N N
+
+
+Console.WriteLine("Lento o veloce? (1, 2)");
 int scelta = int.Parse(Console.ReadLine());
 int slp = 0;
 if (scelta == 1)
@@ -46,9 +65,36 @@ while (true)
     }
 }
 
+
+
+
+///   CCCCC     L         AAAAA    SSSSS   SSSSS   IIIIIII
+///  C          L        A     A  S       S           I  
+///  C          L        AAAAAAA   SSSSS   SSSSS      I  
+///  C          L        A     A        S       S     I  
+///  CCCCC      LLLLLL   A     A   SSSSS   SSSSS   IIIIIII
+
+#region classi
+
+#endregion
+
+
+
+
+
+
+
+///     M       M  EEEEEEE  TTTTTTT  OOOOO   DDDDD    IIIIIII
+///     MM     MM  E           T    O     O  D    D      I  
+///     M M   M M  EEEE        T    O     O  D     D     I  
+///     M  M  M M  E           T    O     O  D    D      I  
+///     M   M   M  EEEEEEE     T     OOOOO   DDDDD    IIIIIII
+
+#region metodi
 int rnd()
 {
     Random rnd = new Random();
     int num = rnd.Next(1, 5);
     return num;
 }
+#endregion
