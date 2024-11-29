b461f493fb571f2b42027be68bc6da818eb4e99d
Paolo Bertinetti <bertinetti@sirius-ea.com>
2024-11-29 10:44:14 +0100

    Aggiungere i file di progetto.

---
+++ Edneennre/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Lento o veloce? (1, 2)");
+int scelta = int.Parse(Console.ReadLine());
+int slp = 0;
+if (scelta == 1)
+{
+    slp = 500;
+}
+else if (scelta == 2)
+{
+    slp = 0;
+}
+else
+{
+    Console.WriteLine("Scelta non valida");
+    return;
+}
+
+
+
+while (true)
+{
+    switch (rnd())
+    {
+        case 1:
+            Console.Write("eden");
+            Thread.Sleep(slp);
+            break;
+        case 2:
+            Console.Write("red");
+            Thread.Sleep(slp);
+            break;
+        case 3:
+            Console.Write("edenred");
+            Thread.Sleep(slp);
+            break;
+        case 4:
+            Console.Write("dender");
+            Thread.Sleep(slp);
+            break;
+        case 5:
+            Console.Write("rendd");
+            Thread.Sleep(slp);
+            break;
+        default:
+            break;
+    }
+}
+
+int rnd()
+{
+    Random rnd = new Random();
+    int num = rnd.Next(1, 5);
+    return num;
+}
